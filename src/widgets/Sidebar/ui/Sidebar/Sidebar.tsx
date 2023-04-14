b7d012f9ea8690ba14fa53d189e9b78bc0bf86fc
e.fedorov <e.fedorov@bivgroup.ru>
2023-04-14 12:18:44 +0300

    added: sidebar

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,32 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './Sidebar.module.scss';
+import React, {useState} from "react";
+import {Button} from "shared/ui/Button/Button";
+import {ThemeSwitcher} from "widgets/ThemeSwitcher";
+
+interface SidebarProps {
+    className?: string;
+}
+
+export const Sidebar = ({className}: SidebarProps) => {
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
+    const onToggle = () => {
+        setIsCollapsed(prev => !prev);
+    }
+
+    return (
+        <div
+            className={classNames(cls.Sidebar, {[cls.collapsed]: isCollapsed}, [className])}
+        >
+            <Button onClick={onToggle}>
+                toggle
+            </Button>
+
+            <div className={cls.switchers}>
+                <ThemeSwitcher />
+                {/* LangSwitcher */}
+            </div>
+        </div>
+    )
+}
