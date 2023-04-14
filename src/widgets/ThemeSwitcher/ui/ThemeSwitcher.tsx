3f6503628a38d8fabebfdb297f59ebe817ac7960
e.fedorov <e.fedorov@bivgroup.ru>
2023-04-14 11:30:54 +0300

    added: theme switcher

---
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -0,0 +1,24 @@
+import {classNames} from 'shared/lib/classNames/classNames';
+import cls from './ThemeSwitcher.module.scss';
+import React from "react";
+import {useTheme} from "app/providers/ThemeProvider";
+import ThemesIcon from 'shared/assets/icons/ThemesIcon.svg';
+import {Button, ThemeButton} from "shared/ui/Button/Button";
+
+interface ThemeSwitcherProps {
+    className?: string;
+}
+
+export const ThemeSwitcher = ({className}: ThemeSwitcherProps) => {
+    const {toggleTheme} = useTheme();
+
+    return (
+        <Button
+            theme={ThemeButton.CLEAR}
+            className={classNames(cls.ThemeSwitcher, {}, [className])}
+            onClick={toggleTheme}
+        >
+            <ThemesIcon className={classNames(cls.theme, {}, [className])} />
+        </Button>
+    )
+}
