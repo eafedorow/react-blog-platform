5bfa522b2e9162906a8134388cdbc10b5ace1832
Vyacheslav.Sazonov <vsazonov@bivgroup.ru>
2023-04-20 16:27:53 +0300

    fixed linting in test

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -3,6 +3,7 @@ import { Button, ThemeButton } from 'shared/ui/Button/Button';
 
 describe('Button', () => {
 	test('Test render', () => {
+		// eslint-disable-next-line i18next/no-literal-string
 		render(<Button>TEST</Button>);
 		expect(screen.getByText('TEST')).toBeInTheDocument();
 	});
